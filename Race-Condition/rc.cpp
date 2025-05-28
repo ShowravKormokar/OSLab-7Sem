17f04b90326c03fa71ab1dd0a7de939caf82ea5d
ShowravKormokar <showravkormokar354@gmail.com>
2025-05-28 20:10:15 +0600

    Modify race condition to make it more standared

--- Race-Condition/rc.cpp
+++ Race-Condition/rc.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int p1(int a)
+{
+	return a += 1;
+}
+
+int p2(int a)
+{
+	return a += 1;
+}
+
 int main()
 {
-    // Initial value of a
-    int a = 10;
+	// Initial value of a
+	int a = 10;
 
-    int p1 = a; // Step1: Process 1 read a=10
-    int p2 = a; // Step2: Process 2 read a=10
+	int temp1 = a; // Step1: Process 1 read a=10
+	int temp2 = a; // Step2: Process 2 read a=10
 
-    p1 += 1; // Step3: Process 1 increase value 1
-    a = p1;  // Step4: Process 1 set updated value of a
-    cout << "P1 is: " << p1 << endl;
+	// Step3: Process 1 increase value 1
+	a = p1(temp1); // Step4: Process 1 set updated value of a
+	cout << "P1 is: " << a << endl;
 
-    p2 += 1; // Step5: Process 2 increase value 1
-    a = p2;  // Step6: Process 2 set updated value of a
-    cout << "P2 is: " << p2 << endl;
+	// Step5: Process 2 increase value 1
+	a = p2(temp2); // Step6: Process 2 set updated value of a
+	cout << "P2 is: " << a << endl;
 
-    cout << "Updated value of a: " << a << "\t(Race Condition)" << endl;
-    cout << "Expected value of a: 12" << endl;
+	cout << "Updated value of a: " << a << "\t(Race Condition)" << endl;
+	cout << "Expected value of a: 12" << endl;
 
-    return 0;
+	return 0;
 }
